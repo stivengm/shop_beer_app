d9424b1503c56d39d9f381ea5a978c2afdd2d0d8
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-12 18:18:25 -0500

    FIX - Ajuste de form_login.

---
+++ lib/ui/views/user/login_view/form_login.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:email_validator/email_validator.dart';
+import 'package:shop_beer_app/core/blocs/login/login_bloc.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
+import 'package:shop_beer_app/ui/app_style.dart';
+import 'package:shop_beer_app/ui/widgets/primary_button.dart';
+
+class FormLogin extends StatefulWidget {
+  const FormLogin({ Key? key }) : super(key: key);
+
+  @override
+  State<FormLogin> createState() => _FormLoginState();
+}
+
+class _FormLoginState extends State<FormLogin> {
+  bool _passwordVisible = true;
+  final formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    _passwordVisible = true;
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            textInputAction: TextInputAction.next,
+            controller: emailController,
+            keyboardType: TextInputType.emailAddress,
+            autocorrect: false,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (email) => email != null && !EmailValidator.validate(email) ? 'Ingrese un email válido' : null,
+            decoration: InputDecoration(
+              labelText: 'Email',
+              labelStyle: const TextStyle(
+                color: primaryColor
+              ),
+              filled: true,
+              fillColor: whiteColor,
+              focusedBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: whiteColor
+                ),
+                borderRadius: BorderRadius.circular(10.0)
+              )
+            ),
+          ),
+          const SizedBox(height: 15.0),
+          TextFormField(
+            obscureText: _passwordVisible,
+            controller: passwordController,
+            autocorrect: false,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (pass) => pass != null && pass.length < 6 ? 'Ingrese mínimo 6 caracteres' : null,
+            decoration: InputDecoration(
+              labelText: 'Contraseña',
+              labelStyle: const TextStyle(
+                color: primaryColor
+              ),
+              filled: true,
+              fillColor: whiteColor,
+              focusedBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: whiteColor
+                ),
+                borderRadius: BorderRadius.circular(10.0)
+              ),
+              suffixIcon: IconButton(
+                icon: Icon(
+                  _passwordVisible
+                  ? Icons.visibility_outlined
+                  : Icons.visibility_off_outlined
+                ),
+                onPressed: () {
+                  setState(() {
+                    _passwordVisible = !_passwordVisible;
+                  });
+                },
+              )
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          const Text("Ha olvidado la contraseña?"),
+          const SizedBox(height: 20.0),
+          PrimaryButton(
+            text: 'Siguiente',
+            onPressed: signIn
+          ),
+          const SizedBox(height: 30.0),
+          RichText(
+            text: TextSpan(
+              text: 'No tienes una cuenta?',
+              style: const TextStyle(
+                color: blackColor
+              ),
+              children: [
+                TextSpan(
+                  text: ' Regístrate',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                  recognizer: _registro()
+                )
+              ]
+            )
+          )
+        ],
+      ),
+    );
+  }
+
+  Future signIn() async {
+    final isValidForm = formKey.currentState!.validate();
+    if (!isValidForm) return;
+
+    // try {
+    //   final loginBloc = BlocProvider.of<LoginBloc>(context);
+    //   final response = await FirebaseAuth.instance.signInWithEmailAndPassword(
+    //     email: emailController.text.trim(), 
+    //     password: passwordController.text.trim()
+    //   );
+    //   // response.user;
+    //   // loginBloc.add( InfoUser(response.user) );
+    //   loginBloc.add( const IsLogguedUser(true) );
+    //   print(response);
+    // } on FirebaseException catch (e) {
+    //   NotificationsWidget(message: e.message!,).showNotificationError(context);
+    //   return;
+    // }
+
+    Navigator.of(context).pushNamedAndRemoveUntil('home', (route) => false);
+
+  }
+
+  TapGestureRecognizer _registro() {
+
+    return TapGestureRecognizer()..onTap = () {
+      Navigator.pushNamed(context, 'register');
+    };
+
+  }
+}
