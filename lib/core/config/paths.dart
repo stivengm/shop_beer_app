21234f88e69eca0bf3f2d0af643012fdffbfa141
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-12 18:08:07 -0500

    FIX - Ajuste de loginView.

--- lib/core/config/paths.dart
+++ lib/core/config/paths.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shop_beer_app/ui/views/home_view/home_view.dart';
+import 'package:shop_beer_app/ui/views/user/login_view/login_view.dart';
 
 Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
-  'home': ( _ ) => const HomeView()
+  'home': ( _ ) => const HomeView(),
+  'login': ( _ ) => const LoginView()
 };
--- lib/ui/views/user/login_view/login_view.dart
+++ lib/ui/views/user/login_view/login_view.dart
@@ -5,6 +5,20 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        child: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: SingleChildScrollView(
+            child: Stack(
+              children: [
+                
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
