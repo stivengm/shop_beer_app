9ebbd9f067d58b43bef25ac250ec14770369ecb7
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-13 12:46:40 -0500

    FIX - Ajuste de historyView.

--- lib/core/config/paths.dart
+++ lib/core/config/paths.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shop_beer_app/ui/views/cart_view/cart_view.dart';
 import 'package:shop_beer_app/ui/views/favorites_view/favorites_view.dart';
 import 'package:shop_beer_app/ui/views/home_view/home_view.dart';
+import 'package:shop_beer_app/ui/views/user/history_view/history_view.dart';
 import 'package:shop_beer_app/ui/views/user/information_view/information_view.dart';
 import 'package:shop_beer_app/ui/views/user/login_view/login_view.dart';
 import 'package:shop_beer_app/ui/views/user/register_view/register_view.dart';
@@ -12,5 +13,6 @@ Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
   'register': ( _ ) => const RegisterView(),
   'information': ( _ ) => const InformationView(),
   'cart': ( _ ) => const CartView(),
-  'favorites': ( _ ) => const FavoritesView()
+  'favorites': ( _ ) => const FavoritesView(),
+  'history': ( _ ) => const HistoryView()
 };
