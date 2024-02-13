4d8b7f432211e23a68085483ba4daab794a9d6e2
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-12 21:04:13 -0500

    ADD - Se añade menu drawer.

---
+++ lib/ui/drawers/home_drawer.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shop_beer_app/data/models/menu_items.dart';
+
+class HomeMenu extends StatefulWidget {
+
+  final GlobalKey<ScaffoldState> scaffoldKey;
+
+
+  const HomeMenu({super.key, required this.scaffoldKey});
+
+  @override
+  State<HomeMenu> createState() => _HomeMenuState();
+}
+
+class _HomeMenuState extends State<HomeMenu> {
+  int navDrawerIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationDrawer(
+      selectedIndex: navDrawerIndex,
+      onDestinationSelected: (value) {
+        setState(() {
+          navDrawerIndex = value;
+        });
+
+        final menuItem = appMenuItems[value];
+        Navigator.pushNamed(context, menuItem.link);
+        widget.scaffoldKey.currentState?.closeDrawer();
+      },
+      children: [
+        ...appMenuItems.map((item) => NavigationDrawerDestination(
+          icon: Icon( item.icon ), label: Text(item.title))
+        )
+      ]
+    );
+  }
+}
