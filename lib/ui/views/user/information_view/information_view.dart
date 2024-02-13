699338fd25f07870715a65b00b3f4db673282769
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-13 12:22:27 -0500

    FIX - Ajuste de informationView.

--- lib/ui/views/user/information_view/information_view.dart
+++ lib/ui/views/user/information_view/information_view.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:shop_beer_app/ui/views/user/information_view/widgets/header_information.dart';
 
 class InformationView extends StatelessWidget {
   const InformationView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const List<String> list = <String>['Portal de Molinos 3', 'Two', 'Three', 'Four'];
+    Size media = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Text("Información"),
-          Text("Lugar de entrega:"),
-          Text("Más información sobre la entrega"),
-        ],
-      ),
+      body: SizedBox(
+        height: double.infinity,
+        child: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("Información"),
+                  const HeaderInformation(title: "Lugar de entrega"),
+                  DropdownMenu<String>(
+                    initialSelection: list.first,
+                    width: (media.width * 1) - 40,
+                    onSelected: (String? value) {
+                      // setState
+                      print(value);
+                    },
+                    dropdownMenuEntries: list.map<DropdownMenuEntry<String>>((String value) {
+                      return DropdownMenuEntry<String>(value: value, label: value);
+                    }).toList(),
+                  ),
+                  
+                  const HeaderInformation(title: "Más información sobre la entrega"),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder()
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      )
     );
   }
 }
