fb58f32bebbab4a7bc13fb0bc1d4b43c283f879c
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-12 21:03:53 -0500

    FIX - Ajuste de homeView.

--- lib/ui/views/home_view/home_view.dart
+++ lib/ui/views/home_view/home_view.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:shop_beer_app/ui/drawers/home_drawer.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(),
+      drawer: HomeMenu(scaffoldKey: scaffoldKey),
+      body: const Center(
+        child: Text("Body"),
+      )
+    );
   }
 }
