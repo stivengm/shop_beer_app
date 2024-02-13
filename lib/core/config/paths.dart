79a93a1203ab899f4a97fcb99ccb39ac8e67fe85
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-13 18:14:33 -0500

    FIX - Ajuste de notifications.

--- lib/core/config/paths.dart
+++ lib/core/config/paths.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shop_beer_app/ui/views/cart_view/cart_view.dart';
 import 'package:shop_beer_app/ui/views/favorites_view/favorites_view.dart';
 import 'package:shop_beer_app/ui/views/home_view/home_view.dart';
+import 'package:shop_beer_app/ui/views/notifications_view/notifications_view.dart';
 import 'package:shop_beer_app/ui/views/user/history_view/history_view.dart';
 import 'package:shop_beer_app/ui/views/user/information_view/information_view.dart';
 import 'package:shop_beer_app/ui/views/user/login_view/login_view.dart';
@@ -14,5 +15,6 @@ Map<String, WidgetBuilder> routesApp() => <String, WidgetBuilder> {
   'information': ( _ ) => const InformationView(),
   'cart': ( _ ) => const CartView(),
   'favorites': ( _ ) => const FavoritesView(),
-  'history': ( _ ) => const HistoryView()
+  'history': ( _ ) => const HistoryView(),
+  'notifications': ( _ ) => const NotificationsView()
 };
