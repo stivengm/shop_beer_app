2440922d2d05e62884db2f97969ae17f6774b178
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-13 13:57:08 -0500

    FIX - Ajuste de cartView.

--- lib/ui/views/cart_view/cart_view.dart
+++ lib/ui/views/cart_view/cart_view.dart
@@ -5,6 +5,8 @@ class CartView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
