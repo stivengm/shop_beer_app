c5fc813b4b9fec4b06d5f60501cc847bbbefe284
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-12 21:45:46 -0500

    ADD - homeBloc from getMethosPay.

--- lib/ui/views/home_view/home_view.dart
+++ lib/ui/views/home_view/home_view.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_beer_app/core/blocs/home/home_bloc.dart';
 import 'package:shop_beer_app/ui/drawers/home_drawer.dart';
+import 'package:shop_beer_app/ui/views/home_view/widgets/medios_pay.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+
+  @override
+  void initState() {
+    final homeBloc = BlocProvider.of<HomeBloc>(context);
+    homeBloc.getMethodsPay();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +28,10 @@ class HomeView extends StatelessWidget {
       key: scaffoldKey,
       appBar: AppBar(),
       drawer: HomeMenu(scaffoldKey: scaffoldKey),
-      body: const Center(
-        child: Text("Body"),
+      body: const Column(
+        children: [
+          MediosPay()
+        ],
       )
     );
   }
