2f4b25ffe1a7187fd9105a30cdfd77ee504d00b2
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-12 22:55:46 -0500

    FIX - Methods pay.

--- lib/ui/views/home_view/widgets/medios_pay.dart
+++ lib/ui/views/home_view/widgets/medios_pay.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shop_beer_app/data/models/methods_pay_model.dart';
+import 'package:shop_beer_app/ui/app_style.dart';
 
 class MediosPay extends StatelessWidget {
-  const MediosPay({super.key});
+  final List<MethodsPayModel>? methodsPay;
+
+  const MediosPay({super.key, required this.methodsPay});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+    return Container(
+      padding: const EdgeInsets.all(defaultPadding),
+      width: double.infinity,
       child: Column(
         children: [
-          Text("Nuestros medios de pago")
+          Text("Nuestros métodos de pago"),
+          Container(
+            padding: const EdgeInsets.all(3),
+            width: double.infinity,
+            height: 85.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: methodsPay!.length,
+              itemBuilder: ( BuildContext context, int index ) => Container(
+                margin: const EdgeInsets.only(right: 5.0),
+                width: 100.0,
+                child: Image.network(methodsPay![index].img!, fit: BoxFit.contain,),
+              )
+            ),
+          )
         ],
       ),
     );
