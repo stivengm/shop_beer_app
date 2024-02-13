e04a63d17c54fbaf267f34da865d3452e7809d61
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-13 13:26:36 -0500

    FIX - Ajuste de history.

--- lib/ui/views/user/history_view/history_view.dart
+++ lib/ui/views/user/history_view/history_view.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shop_beer_app/ui/app_style.dart';
+import 'package:shop_beer_app/ui/views/user/history_view/widgets/history_card.dart';
 
 class HistoryView extends StatelessWidget {
   const HistoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: backgroundApp,
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+          child: Column(
+            children: [
+              HistoryCard(),
+              HistoryCard(),
+              
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
