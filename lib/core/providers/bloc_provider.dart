0261856b4b81767788e84c82f9896457da9f7802
Stiven Gomez <stiven.gomez@nuvu.cc>
2024-02-13 19:03:43 -0500

    FIX - Ajuste de notificationsBloc.

--- lib/core/providers/bloc_provider.dart
+++ lib/core/providers/bloc_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_beer_app/core/blocs/home/home_bloc.dart';
 import 'package:shop_beer_app/core/blocs/login/login_bloc.dart';
+import 'package:shop_beer_app/core/blocs/notifications/notifications_bloc.dart';
 
 final List<BlocProvider> providerBloc = [
   BlocProvider<HomeBloc>(create: (_) => HomeBloc()),
   BlocProvider<LoginBloc>(create: (_) => LoginBloc()),
+  BlocProvider<NotificationsBloc>(create: (_) => NotificationsBloc()),
 ];
